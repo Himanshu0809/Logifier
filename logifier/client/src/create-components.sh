6cc102ef6438a7693ea73830053569273d873231
Ritesh1729 <ritesh.kumar465@gmail.com>
2021-05-23 10:35:49 +0530

    boilerplate for creating component

---
+++ logifier/client/src/create-components.sh
@@ -0,0 +1,10 @@
+echo "Enter component name"
+read component_name
+echo "Do you want to include test files (y/n)"
+read include_test_files
+mkdir -p Components/$component_name && cd $_
+touch $component_name.js $component_name.styles.js
+if echo "$include_test_files" | grep -iq "^[yY]"; then
+	touch $component_name.test.js
+fi
+echo "Done ..."
