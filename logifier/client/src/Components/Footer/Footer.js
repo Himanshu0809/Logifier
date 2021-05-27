6b312981bb5b95de25ad1b656ecc1e45f27595d5
Himanshu Gupta <guptahimanshu479@gmail.com>
2021-05-28 01:55:37 +0530

    Create Footer Component

---
+++ logifier/client/src/Components/Footer/Footer.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { AppBar, Toolbar, Typography, Container } from "@material-ui/core";
+import { FooterContent } from './Footer.styles.js';
+
+function Footer() {
+  return (
+    <AppBar position="static" color="primary">
+      <Container maxWidth="md">
+        <Toolbar>
+          <Typography variant="body1" color="inherit">
+            <FooterContent>© 2021 Logifier by Ritesh & Himanshu. All Rights Reserved</FooterContent>
+          </Typography>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
+
+export default Footer;
